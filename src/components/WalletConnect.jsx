4c222744d1a66ff08f8e01f9c9a60c51c157d8fd
Atroopa <atroopa@gmail.com>
2023-03-10 20:22:50 +0330

    walletconnect card ready for backend

--- src/components/WalletConnect.jsx
+++ src/components/WalletConnect.jsx
@@ -3,19 +3,19 @@ import React, {useState} from 'react';
 const WalletConnect = () => {
   return (
 
-            <div className="p-10 m-8 w-[750px] bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+            <div className="flex flex-col items-center justify-center p-10 m-8 w-[750px] bg-[#303445] rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
                 <div>
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight font-extrabold text-transparent text-2xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 dark:text-white">
+                    <h5 className="py-5 text-5xl bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 inline-block text-transparent bg-clip-text dark:text-white">
                         Wallect Address
                     </h5>
                 </div>
 
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                <p className="py-5 mb-3 font-normal text-1xl bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 inline-block text-transparent bg-clip-text dark:text-gray-400">
                 0x6dAEO....f839B
                 </p>
 
-                <button class="px-6 py-2 text-gray-100 rounded bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
-                    Connect
+                <button type="button" class="w-[220px] text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+                    connect
                 </button>
             </div>
 
