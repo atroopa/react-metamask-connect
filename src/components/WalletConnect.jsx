2c3f3ea3e98752e49bc8b79f39ee68c3124b8ccb
Atroopa <atroopa@gmail.com>
2023-03-12 12:28:33 +0330

    walletConnect ready for use state coding

--- src/components/WalletConnect.jsx
+++ src/components/WalletConnect.jsx
@@ -11,10 +11,10 @@ const WalletConnect = () => {
                 </div>
 
                 <p className="py-5 mb-3 font-normal text-1xl bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 inline-block text-transparent bg-clip-text dark:text-gray-400">
-                0x6dAEO....f839B
+                Address : 0x6dAEO....f839B
                 </p>
 
-                <button type="button" class="w-[220px] text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+                <button type="button" className="w-[220px] text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
                     connect
                 </button>
             </div>
