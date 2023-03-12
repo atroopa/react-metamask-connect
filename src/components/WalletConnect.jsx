68ed77fa9db742ba9bac652d6836363b7b24d017
Atroopa <atroopa@gmail.com>
2023-03-12 13:49:19 +0330

    fix not work connect button

--- src/components/WalletConnect.jsx
+++ src/components/WalletConnect.jsx
@@ -51,7 +51,7 @@ const WalletConnect = () => {
                 Address : {defualtAccount}
                 </p>
 
-                <button type="button" className="w-[220px] text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+                <button onClick={connectWalletHandler} type="button" className="w-[220px] text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
                     connect
                 </button>
             </div>
